66992c55d644afa23fc00b3b190238b198f2055c
Husnain Kareem <husnain446@gmail.com>
2015-03-10 12:52:03 +0500

    Save Service Settings To Work intelligent

--- app/src/main/java/com/byteshaft/iblinklednotification/MainActivity.java
+++ app/src/main/java/com/byteshaft/iblinklednotification/MainActivity.java
@@ -1,7 +1,9 @@
 package com.byteshaft.iblinklednotification;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -35,10 +37,18 @@ public class MainActivity extends ActionBarActivity implements Switch.OnCheckedC
                 if (mSwitch.isChecked()) {
                     if (FlashlightService.getInstance() == null) {
                         startService(new Intent(this, FlashlightService.class));
+                        saveServiceStateEnabled(true);
                     }
                 } else {
                     stopService(new Intent(this, FlashlightService.class));
+                    saveServiceStateEnabled(false);
                 }
         }
+    }
+
+    private void saveServiceStateEnabled(boolean ENABLED) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.edit().putBoolean("enabled", ENABLED).commit();
+
     }
 }
--- app/src/main/java/com/byteshaft/iblinklednotification/PhoneBootStateReader.java
+++ app/src/main/java/com/byteshaft/iblinklednotification/PhoneBootStateReader.java
@@ -3,10 +3,20 @@ package com.byteshaft.iblinklednotification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class PhoneBootStateReader extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, FlashlightService.class));
+        if (isServiceSettingEnabled(context)) {
+            context.startService(new Intent(context, FlashlightService.class));
+        }
+    }
+
+    private boolean isServiceSettingEnabled(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getBoolean("enabled", false);
     }
 }
